cf9eee4dc391e5f1e38d3cf39ed8706938c758b0
osmirmariano <osmirmariano@uft.edu.br>
2017-03-10 00:36:14 -0300

    Implementação para acessar os contatos do telefone

--- Agenda/www/js/index.js
+++ Agenda/www/js/index.js
@@ -1,38 +1,45 @@
-
-function errorHandler(e){
+function errorHandler(e)
+{
     console.log(e);
 }
 
-function gotContacts(contacts){
+function gotContacts(contacts)
+{
     var list = $("#contact-list").append('<ul></ul>').find('ul');
     $.each(contacts, function(index, value){
-        if(value.displayName && value.phoneNumbers){
-            if(value.photos){
-                list.append('<li>' + value.displayName + ' - ' + value.phoneNumbers[0].value + '<img src="' + value.photos[0].value + '">' + '</li>');
+        if(value.displayName && value.phoneNumbers)
+        {
+            if(value.photos)
+            {
+                list.append('<li>' + value.displayName + ' - ' + value.phoneNumbers[0].value + '<img src="' + value.photos[0].value + '">' +'</li>');
+                
             }
-            else{
-                list.append('<li>' + value.displayName + ' - ' + value.phoneNumbers[0].value + '</li>'); 
+            else
+            {
+                list.append('<li>' + value.displayName + ' - ' + value.phoneNumbers[0].value +'</li>');
             }
+
         }
-    });
+    }); 
 }
 
 var app = {
-    // Application Constructor
-    initialize: function() {
+
+    initialize: function()
+    {
+
         this.bindEvents();
     },
 
-    bindEvents: function(){
-        document.addEventListener('deviceready', this.ondDeviceReady, false);
+    bindEvents: function()
+    {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
 
-
-    onDeviceReady: function() {
+    onDeviceReady: function()
+    {
         navigator.contacts.find([navigator.contacts.fieldType.displayName], gotContacts, errorHandler);
     },
 };
 
 
-            app.initialize();
- 
