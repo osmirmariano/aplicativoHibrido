cc4740b0ca87da6a4a3d9784a5a7a5a3b2cf700a
osmirmariano <osmirmariano@uft.edu.br>
2017-03-10 00:09:37 -0300

    Criando aplicação com contatos

---
+++ Agenda/platforms/android/assets/www/js/index.js
@@ -0,0 +1,38 @@
+
+function errorHandler(e){
+    console.log(e);
+}
+
+function gotContacts(contacts){
+    var list = $("#contact-list").append('<ul></ul>').find('ul');
+    $.each(contacts, function(index, value){
+        if(value.displayName && value.phoneNumbers){
+            if(value.photos){
+                list.append('<li>' + value.displayName + ' - ' + value.phoneNumbers[0].value + '<img src="' + value.photos[0].value + '">' + '</li>');
+            }
+            else{
+                list.append('<li>' + value.displayName + ' - ' + value.phoneNumbers[0].value + '</li>'); 
+            }
+        }
+    });
+}
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function(){
+        document.addEventListener('deviceready', this.ondDeviceReady, false);
+    },
+
+
+    onDeviceReady: function() {
+        navigator.contacts.find([navigator.contacts.fieldType.displayName], gotContacts, errorHandler);
+    },
+};
+
+
+            app.initialize();
+ 
